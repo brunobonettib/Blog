5efddc527aa4a17c27ee4f49af74b4df1a5cf072
Bruno <bruno.bonetti.b@gmail.com>
2022-02-03 20:57:36 -0300

    RoleClaimsExtensionAdd

--- Services/TokenService.cs
+++ Services/TokenService.cs
@@ -13,14 +13,10 @@ public class TokenService
     {
         var tokenHandler = new JwtSecurityTokenHandler();
         var key = Encoding.ASCII.GetBytes(Configuration.JwtKey);
+        var claims = user.GetClaims();
         var tokenDescriptor = new SecurityTokenDescriptor
         {
-            Subject = new ClaimsIdentity(new Claim[]
-            {
-                new Claim(ClaimTypes.Name, "BrunoBonetti"),
-                new Claim(ClaimTypes.Role, "admin"),
-                new Claim("fruta", "banana")
-            }),
+            Subject = new ClaimsIdentity(claims),
             Expires = DateTime.UtcNow.AddHours(8),
             SigningCredentials = new SigningCredentials(
                 new SymmetricSecurityKey(key),
